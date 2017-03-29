21a8919fa0b4fb426f6c3c325a4808beaaa6a8ec
Dave Moten <davidmoten@gmail.com>
2017-03-30 07:25:02 +1100

    dev

--- src/main/java/org/davidmoten/gt/btree/ro/Creator.java
+++ src/main/java/org/davidmoten/gt/btree/ro/Creator.java
@@ -1,7 +1,9 @@
 package org.davidmoten.gt.btree.ro;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.stream.Collectors;
 
 import com.github.davidmoten.rx2.flowable.Transformers;
@@ -16,14 +18,16 @@ public class Creator<Entry, Key> {
     private final int maxPageSizeBytes;
     private final int nodeMaxChildren;
     private final Function<Entry, Key> keyMapper;
+    private final Callable<File> fileFactory;
 
     public Creator(Serializer<Key> keySerializer, Serializer<Entry> entrySerializer, int maxPageSizeBytes,
-            int nodeMaxChildren, Function<Entry, Key> keyMapper) {
+            int nodeMaxChildren, Function<Entry, Key> keyMapper, Callable<File> fileFactory) {
         this.keySerializer = keySerializer;
         this.entrySerializer = entrySerializer;
         this.maxPageSizeBytes = maxPageSizeBytes;
         this.nodeMaxChildren = nodeMaxChildren;
         this.keyMapper = keyMapper;
+        this.fileFactory = fileFactory;
     }
 
     public List<File> persist(Flowable<Entry> entries) {
@@ -45,10 +49,47 @@ public class Creator<Entry, Key> {
                         System.arraycopy(en.bytes, 0, bytes, i, en.bytes.length);
                         i += en.bytes.length;
                     }
-                    return bytes;
+                    File file = fileFactory.call();
+                    try (FileOutputStream fos = new FileOutputStream(file)) {
+                        fos.write(bytes);
+                    }
+                    int[] position = new int[1];
+                    return list.stream().map(en -> {
+                        Key key;
+                        try {
+                            key = keyMapper.apply(en.entry);
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                        KeyFilePosition<Key> kp = new KeyFilePosition<Key>(key, file.getName(), position[0]);
+                        position[0] += en.bytes.length;
+                        return kp;
+                    }).collect(Collectors.toList());
                 });
-        ;
         return null;
+    }
+
+    private static final class KeyFilePosition<Key> {
+        final Key key;
+        final String filename;
+        final int position;
+
+        KeyFilePosition(Key key, String filename, int position) {
+            this.key = key;
+            this.filename = filename;
+            this.position = position;
+        }
+    }
+
+    private static final class KeyPosition<Key> {
+        final Key key;
+        final int position;
+
+        KeyPosition(Key key, int position) {
+            this.key = key;
+            this.position = position;
+        }
+
     }
 
     private static class SerializedEntry<Entry> {
